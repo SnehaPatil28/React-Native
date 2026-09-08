f8264cb6d70290b30b8711037fbc252594b3d3da
Sneha Patil <snehapatil5581@gmail.com>
2026-09-09 01:11:09 +0530

    Theme file created

---
+++ Exp2/Component/Exp6/theme.ts
@@ -0,0 +1,34 @@
+export const COLORS = {
+  primary: '#2196F3',
+  secondary: '#4CAF50',
+  danger: '#F44336',
+
+  background: '#F5F5F5',
+  card: '#FFFFFF',
+
+  text: '#222222',
+  secondaryText: '#666666',
+
+  border: '#CCCCCC',
+  white: '#FFFFFF',
+};
+
+export const SPACING = {
+  small: 8,
+  medium: 15,
+  large: 20,
+  xlarge: 30,
+};
+
+export const FONT_SIZES = {
+  small: 14,
+  medium: 16,
+  large: 20,
+  xlarge: 26,
+};
+
+export const RADIUS = {
+  small: 8,
+  medium: 10,
+  large: 15,
+};
